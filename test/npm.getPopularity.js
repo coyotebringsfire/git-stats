4b90dfaacc90a26d3bc7b85147384b972a8b40b1
CoyoteBringsFire <aumkara@gmail.com>
2015-07-01 13:49:03 -0700

    added debugging

--- test/npm.getPopularity.js
+++ test/npm.getPopularity.js
@@ -1,29 +1,32 @@
-var debug=require('debug')('npm:getPopularity:test'),
+var debug=require('debug')('npm:popularity:test'),
     should=require('should');
 
-describe("npm:getPopularity", function getPopularity() {
+describe("npm:popularity", function getPopularity() {
   this.timeout(0);
-  var debug=require('debug')('npm:getPopularity:test:getPopularity');
+  var debug=require('debug')('npm:popularity:test:getPopularity');
   it("should return a promise", function itResponse(done) {
-    var debug=require('debug')('npm:getPopularity:test:getPopularity:itResponse');
-    var popularity=require('../lib/npm').getPopularity;
+    var debug=require('debug')('npm:popularity:test:getPopularity:itResponse');
+    var popularity=require('../lib/npm').popularity;
     var promise=popularity();
     promise.then.should.be.type('function');
     done();
   });
-  it.only("should resolve the promise with a hash containing all of the NPM modules, and their PageRank value", function itResponse(done) {
-    var debug=require('debug')('npm:getPopularity:test:getPopularity:itResponse');
-    var popularity=require('../lib/npm').popularity;
+  it("should resolve the promise with a hash containing all of the NPM modules, and their PageRank value", function itResponse(done) {
+    var debug=require('debug')('npm:popularity:test:getPopularity:itResponse');
+    var npm=require('../lib/npm');
+    debug("npm %j", npm);
+    var popularity=npm.popularity;
+    debug("npm.popularity %j", npm.popularity);
     popularity().
       then(function onPopularityPromiseResolve(popularity) {
-        var debug=require('debug')('npm:getPopularity:test:popularity:itResponse:onPopularityPromiseResolve');
+        var debug=require('debug')('npm:popularity:test:popularity:itResponse:onPopularityPromiseResolve');
         popularity.should.be.ok;
         for( k in popularity ) {
           popularity[k].should.be.type('number');
         }
         done();
       }, function onPopularityPromiseRejected(err) {
-        var debug=require('debug')('npm:getPopularity:test:getPopularity:itResponse:onPopularityPromiseRejected');
+        var debug=require('debug')('npm:popularity:test:getPopularity:itResponse:onPopularityPromiseRejected');
         should.fail("popularity promise was rejected");
       });
   });
