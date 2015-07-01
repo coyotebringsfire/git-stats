0ea2bfe06ca0db2366528132bb2621653bba2558
CoyoteBringsFire <aumkara@gmail.com>
2015-07-01 14:47:33 -0700

    lib/git.js

--- test/git.js
+++ test/git.js
@@ -4,19 +4,23 @@ var should=require('should'),
 
 describe("git library", function() {
 	it("should have train method", function(done) {
-		Git.train.should.be.type('function');
+    var git=new Git("");
+		git.train.should.be.type('function');
 		done();
 	});
   it("should have getCommits method", function(done) {
-    Git.getCommits.should.be.type('function');
+    var git=new Git("");
+    git.getCommits.should.be.type('function');
     done();
   });
   it("should have processCommits method", function(done) {
-    Git.processCommits.should.be.type('function');
+    var git=new Git("");
+    git.processCommits.should.be.type('function');
     done();
   });
   it("should have predict method", function(done) {
-    Git.predict.should.be.type('function');
+    var git=new Git("");
+    git.predict.should.be.type('function');
     done();
   });
 });
