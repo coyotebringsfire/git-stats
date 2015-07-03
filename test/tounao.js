10dfb4fd2005557e52d390e5f4216598cf62fb3d
CoyoteBringsFire <aumkara@gmail.com>
2015-07-02 18:23:06 -0700

    verification of passing options to brain

--- test/tounao.js
+++ test/tounao.js
@@ -50,8 +50,36 @@ describe("tounao", function tounaoSuite() {
           done(err);
         });
     });
-    it("should reject the returned promise if an error happens during processing", function doIt(done) { should.fail(); });
-    it("should use brain options passed as second argument", function doIt(done) { should.fail(); });
+    it("should reject the returned promise if an error happens during processing", function doIt(done) { 
+      var Brain=require("../lib/tounao"), brain=new Brain(),
+          debug=require('debug')('xuexi:tounao:tounaoSuite:trainFunction:noErrors:test');
+      brain.train([{input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 }},
+           {input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 }},
+           {input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 }}], { errorThresh:1 })
+        .then(function onResolve(msg) {
+          debug("%j", msg);
+          should.fail;
+          done();
+        }, function onReject(err) {
+          err.message.should.match(/exceeded error threshold/);
+          done(err);
+        });
+   });
+    it("should use brain options passed as second argument", function doIt(done) { 
+      var Brain=require("../lib/tounao"), brain=new Brain(),
+          debug=require('debug')('xuexi:tounao:tounaoSuite:trainFunction:noErrors:test');
+      brain.train([{input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 }},
+           {input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 }},
+           {input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 }}], {errorThresh:0.006})
+        .then(function onResolve(msg) {
+          debug("%j", msg);
+          msg.error.should.be.within(0.005, 0.006);
+          done();
+        }, function onReject(err) {
+          should.fail("promise was rejected");
+          done(err);
+        });
+    });
   });
   describe("#question", function trainFunction() {
     it("should reject the returned promise if a data point isn't passed as the first argument", function doIt(done) { should.fail(); });
