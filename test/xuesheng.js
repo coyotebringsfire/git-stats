1b2e4b2c2be2515fe01a3a79353452728800c7dd
CoyoteBringsFire <aumkara@gmail.com>
2015-07-03 16:32:07 -0700

    refactored so the forked child only processes 1 module

--- test/xuesheng.js
+++ test/xuesheng.js
@@ -5,33 +5,20 @@ var should=require('should'),
 describe("xuesheng", function xueshengSuite() {
   it("should reject the returned promise, if an invalid module is passed via IPC", function doIt(done) {
     var child=child_process.fork('../lib/xuesheng');
-    child.send(["INVALIDMODULE"]);
+    child.send("INVALIDMODULE");
     child.on("message", function onMessage(response) {
       response.module.should.equal("INVALIDMODULE");
       response.err.should.be.ok;
       done();
     });
   });
-  it("should emit an event with module stats for each module that is processed", function doIt(done) {
+  it("should emit an event with module stats the module that is processed", function doIt(done) {
     var child=child_process.fork('../lib/xuesheng'), moduleOnePromise=Q.defer(), moduleTwoPromise=Q.defer();
     child.on('message', function(msg) {
-      if( msg.module === "MODULEONE" ) {
-        moduleOnePromise.resolve(msg.results);
-      }
-      if( msg.module === "MODULETWO" ) {
-        moduleTwoPromise.resolve(msg.results);
-      }
+      msg.module.should.match(/MODULEONE/);
+      done();
     });
-    child.send(["MODULEONE", "MODULETWO"]);
-
-    Q.all([moduleOnePromise.promise, moduleTwoPromise.promise])
-      .then(function onAllResolved(results) {
-        results.length.should.equal(2);
-        done();
-      }, function onAllRejected(err) {
-        should.fail("all promises were not resolved %s", err.message);
-        done();
-      });
+    child.send("MODULEONE");
   });
   it("should emit an error event if a module is passed that is invalid", function doIt(done) {
     var child=child_process.fork('../lib/xuesheng');
@@ -39,7 +26,7 @@ describe("xuesheng", function xueshengSuite() {
       msg.err.should.be.ok;
       done();
     });
-    child.send(["MODULEONE"]);
+    child.send("MODULEONE");
   });
   it("should emit an error if a message is received in the wrong format", function wrongFormat(done) {
     var child=child_process.fork('../lib/xuesheng'),
@@ -48,6 +35,6 @@ describe("xuesheng", function xueshengSuite() {
       msg.err.should.be.ok;
       done();
     });
-    child.send("MODULEONE");
+    child.send(["MODULEONE"]);
   });
 });
