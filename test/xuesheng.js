ce37ba9f9027b4eb4967590bbdc9316f0ce06004
CoyoteBringsFire <aumkara@gmail.com>
2015-07-14 11:02:29 -0700

    refactored to use promise chaining correctly

--- test/xuesheng.js
+++ test/xuesheng.js
@@ -4,33 +4,55 @@ var should=require('should'),
 
 describe("xuesheng", function xueshengSuite() {
   it("should reject the returned promise, if an invalid module is passed via IPC", function doIt(done) {
-    var child=child_process.fork('../lib/xuesheng');
-    child.send("INVALIDMODULE");
+    var child;
+    if( process.cwd().match(/test$/) ) {
+      child=child_process.fork('../lib/xuesheng');
+    } else {
+      child=child_process.fork('./lib/xuesheng');
+    }
+    child.send({module:"INVALIDMODULE"});
     child.on("message", function onMessage(response) {
       response.module.should.equal("INVALIDMODULE");
       response.err.should.be.ok;
       done();
     });
   });
-  it("should emit an event with module stats the module that is processed", function doIt(done) {
-    var child=child_process.fork('../lib/xuesheng'), moduleOnePromise=Q.defer(), moduleTwoPromise=Q.defer();
+  it("should emit an event with module stats for the module that is processed", function doIt(done) {
+    var child;
+    if( process.cwd().match(/test$/) ) {
+      child=child_process.fork('../lib/xuesheng');
+    } else {
+      child=child_process.fork('./lib/xuesheng');
+    }
+    var moduleOnePromise=Q.defer(), moduleTwoPromise=Q.defer();
     child.on('message', function(msg) {
-      msg.module.should.match(/MODULEONE/);
+      msg.module.should.match(/should/);
+      msg.results.should.be.ok;
+      should(msg.err).not.be.ok;
       done();
     });
-    child.send("MODULEONE");
+    child.send({module:"should"});
   });
   it("should emit an error event if a module is passed that is invalid", function doIt(done) {
-    var child=child_process.fork('../lib/xuesheng');
+    var child;
+    if( process.cwd().match(/test$/) ) {
+      child=child_process.fork('../lib/xuesheng');
+    } else {
+      child=child_process.fork('./lib/xuesheng');
+    }
     child.on('message', function(msg) {
       msg.err.should.be.ok;
       done();
     });
-    child.send("MODULEONE");
+    child.send({module:"NONEXISTENTMODULE"});
   });
   it("should emit an error if a message is received in the wrong format", function wrongFormat(done) {
-    var child=child_process.fork('../lib/xuesheng'),
-        debug=require('debug')("xuexi:xuesheng:xueshengSuite:wrongFormat");
+    var debug=require('debug')("xuexi:xuesheng:xueshengSuite:wrongFormat"), child;
+    if( process.cwd().match(/test$/) ) {
+      child=child_process.fork('../lib/xuesheng');
+    } else {
+      child=child_process.fork('./lib/xuesheng');
+    }
     child.on('message', function(msg) {
       msg.err.should.be.ok;
       done();
